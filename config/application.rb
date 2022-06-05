470fd7ecbe98fa536f34ccecc3c633383ff69ff2
JulaB <julia.bazhuhina@gmail.com>
2022-06-04 19:48:59 -0700

    Update app to 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -24,20 +24,21 @@ module Jsearch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.autoload_paths << Rails.root.join('lib')
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
-    config.generators do |g|
-      g.stylesheets   false
-      g.javascripts   false
-      g.helper        false
-      g.channel       assets: false
-    end
+     config.generators.system_tests = nil
+     config.generators do |g|
+       g.stylesheets   false
+       g.javascripts   false
+       g.helper        false
+       g.channel       assets: false
+     end
   end
 end
